4eacd997893ae56459efc1f1818ba2c44e36458d
galicians <galicians@gmail.com>
2014-12-13 23:52:24 +0000

    socket.io added

--- server.js
+++ server.js
@@ -1,21 +1,32 @@
+            // dependencies
 var express = require('express');
 var app = express();
 var http = require('http');
 var path = require('path');
 var bodyParser = require('body-parser');
+var server = require('http').createServer(app);
+var io = require('socket.io').listen(server);
 
+        // server config & middleware
 app.set('port', process.env.PORT || 5000);
 app.engine('html', require('ejs').renderFile);
 app.set('views', __dirname + '/public/partials');
 app.set('view engine', 'html');
 app.use(express.static(path.join(__dirname + '/public')));
-
 app.use(bodyParser.json())
 
+
+            // routes
 app.get('/', function(req, res) {
     res.render('index.html');
 });
 
-http.createServer(app).listen(app.get('port'), function() {
+
+server.listen(app.get('port'), function() {
     console.log('Express server listening on port 5000')
+});
+
+
+io.on('connection', function(socket){
+  console.log('a user connected');
 });
