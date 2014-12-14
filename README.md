8216aa821acb41a2413a851ceb46538006ced431
galicians <galicians@gmail.com>
2014-12-14 20:18:49 +0000

    models v1 & route to save a user

--- README.md
+++ README.md
@@ -3,5 +3,19 @@
 <h3>Stack:</h3>
 
 <h4>Angular</h4>
+
+<h4>Postgress</h4>
 <h4>Sequelize</h4>
 Sequelize is a promise-based Node.js ORM for Postgres, MySQL, SQLite and MariaDB. It features solid transaction support, relations, read replication and more.
+
+Sequelize will automatically add the attributes createdAt and updatedAt to the table, therfore this looks a perfect match for the requiremnt:
+Each model needs a modified and created attribute on them.
+
+I've created a json file to simulate the models in angular before creating the real models in postgres using sequelize.
+I've used the $http built-in service to bring them on the controller and then displaying them in the view.
+Once I've got a clearer picture of what the models should be I've just defined the db schema on the node server.
+
+In order to create a maintainable application, I should put all the database logic into the models folder. In this way when the application gets fired up, sequelize will sync the models with the database and afterwards start the server. This way we don't clutter the application while making use of sequelize's features. I've avoided this step intentionally this step to keep the all the logic on the server. Later on this will be modified following the example on:
+http://sequelizejs.com/articles/express
+
+pending: once you start a chat with someone they shouldn't appear in the friends list anymore but only in the chats list
--- server.js
+++ server.js
@@ -8,31 +8,6 @@ var server = require('http').createServer(app);
 var io = require('socket.io').listen(server);
 var Sequelize = require('sequelize')
 
-          // database connection using sequelize
-if (env === 'production') {
-  var sequelize = new Sequelize(process.env.DATABASE_URL);
-}
-else {
-  var sequelize = new Sequelize('example', process.env.USER, process.env.DB_PASSWORD, {
-      dialect: "postgres",
-      port: 5432
-  });
-}
-
-sequelize
-  .authenticate()
-  .complete(function(err) {
-    if (!!err) {
-      console.log('Unable to connect to the database:', err)
-    } else {
-      console.log('Connection has been established successfully.')
-    }
-  })
-
-sequelize.query("SELECT * FROM USERS").success(function(myTableRows) {
-  console.log(myTableRows)
-})
-
         // server config & middleware
 app.set('port', process.env.PORT || 5000);
 var env = process.env.NODE_ENV || "development";
@@ -40,17 +15,71 @@ app.engine('html', require('ejs').renderFile);
 app.set('views', __dirname + '/public/partials');
 app.set('view engine', 'html');
 app.use(express.static(path.join(__dirname + '/public')));
-app.use(bodyParser.json())
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
+          // database connection-sequelize
+if (env === 'production') {
+  var sequelize = new Sequelize(process.env.DATABASE_URL);
+}
+else {
+  var sequelize = new Sequelize('donation', process.env.USER, process.env.DB_PASSWORD, {
+      dialect: "postgres",
+      port: 5432
+  });
+}
+
+sequelize.query("SELECT * FROM USERS").success(function(myTableRows) {
+  console.log(myTableRows)
+})
+
+var User = sequelize.define('user', {
+  name: Sequelize.STRING,
+  username: Sequelize.STRING,
+  password: Sequelize.TEXT,
+  chats: Sequelize.INTEGER
+    });
+
+ var Relationship = sequelize.define('relationship', {
+    userid: Sequelize.INTEGER,
+    friendid: Sequelize.INTEGER
+  });
+
+
+
+
+          // sequelize.sync();
+sequelize
+  .sync({ force: true })
+  .complete(function(err) {
+     if (!!err) {
+       console.log('An error occurred while creating the table:', err)
+     } else {
+       console.log('It worked!')
+     }
+  })
 
             // routes
 app.get('/', function(req, res) {
     res.render('index.html');
 });
 
+app.post('/newuser', function(req, res) {
+  console.log(req.body)
+  var user = req.body;
+    User.create({ 
+     name: user.name,
+     username: user.username,
+     password: user.password,
+     chats: user.chats
+                      })
+});
 
+
+
+        // server 
 server.listen(app.get('port'), function() {
-    console.log('Express server listening on port 5000')
+    console.log('Express server listening on port, ', app.get('port'))
 });
 
 
