f6b5b2945c5e6e9a44b0abe2e3422259fc7f6c86
galicians <galicians@gmail.com>
2014-12-14 15:09:05 +0000

    psql database connected to node server through sequelize

--- server.js
+++ server.js
@@ -6,9 +6,36 @@ var path = require('path');
 var bodyParser = require('body-parser');
 var server = require('http').createServer(app);
 var io = require('socket.io').listen(server);
+var Sequelize = require('sequelize')
+
+          // database connection using sequelize
+if (env === 'production') {
+  var sequelize = new Sequelize(process.env.DATABASE_URL);
+}
+else {
+  var sequelize = new Sequelize('example', process.env.USER, process.env.DB_PASSWORD, {
+      dialect: "postgres",
+      port: 5432
+  });
+}
+
+sequelize
+  .authenticate()
+  .complete(function(err) {
+    if (!!err) {
+      console.log('Unable to connect to the database:', err)
+    } else {
+      console.log('Connection has been established successfully.')
+    }
+  })
+
+sequelize.query("SELECT * FROM USERS").success(function(myTableRows) {
+  console.log(myTableRows)
+})
 
         // server config & middleware
 app.set('port', process.env.PORT || 5000);
+var env = process.env.NODE_ENV || "development";
 app.engine('html', require('ejs').renderFile);
 app.set('views', __dirname + '/public/partials');
 app.set('view engine', 'html');
