f45fab1b3635843badd8f36715a550af4ccdb225
galicians <galicians@gmail.com>
2014-12-15 01:53:33 +0000

    env url

--- server.js
+++ server.js
@@ -22,7 +22,9 @@ console.log('process.env.DATABASE_URL',process.env.DATABASE_URL);
 
           // database connection-sequelize
 if (env === 'production') {
-  var sequelize = new Sequelize(process.env.DATABASE_URL);
+  var sequelize = new Sequelize(process.env.DATABASE_URL, {
+      dialect: "postgres"
+  });
 }
 else {
   var sequelize = new Sequelize('donation', process.env.USER, process.env.DB_PASSWORD, {
